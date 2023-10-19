185c9fe6e3ad6904c320801d568714cbc8aa3656
TeresaLiang123 <teresaliangmain@gmail.com>
2023-10-19 15:59:47 -0400

    finished

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,31 +1,32 @@
 import { Link } from "react-router-dom";
 import db from "../Database";
 import '../index.css'
-
 function Dashboard() {
   const courses = db.courses;
   const source = "../../images/react.png"
   return (
-    <>
-      <h1 className="px-3">Dashboard</h1>
-      <hr width="100%"/>
-        <div className="col- d-flex flex-wrap flex-row">
-            {courses.map((course) => (
-              <div className="px-3 py-3">
-                <Link key={course._id} to={`/Kanbas/Courses/${course._id}/Home`} className="list-group-item">
-                  <div class="card card-size">
-                      <img class="card-img-top" src={source}></img>
-                      <div class='card-body'>
-                        <h6 class="card-title">{course.number} {course.name} {course.startDate} {course.endDate}</h6>
-                        <p class="card-text">Fall 2023 Semester Fall Term</p>
-                      </div>
-                  </div>
-                </Link>
-              </div>
+    <tr>
+      <td width="100%" height="100%">
+        <h1 className="px-3">Dashboard</h1>
+        <hr width="100%"/>
+          <div className="col- d-flex flex-wrap flex-row">
+              {courses.map((course) => (
+                <div className="px-3 py-3">
+                  <Link key={course._id} to={`/Kanbas/Courses/${course._id}/Home`} className="list-group-item">
+                    <div className="card card-size">
+                        <img className="card-img-top" src={source}></img>
+                        <div className='card-body'>
+                          <h6 className="card-title">{course.number} {course.name} {course.startDate} {course.endDate}</h6>
+                          <p className="card-text">Fall 2023 Semester Fall Term</p>
+                        </div>
+                    </div>
+                  </Link>
+                </div>
 
-            ))}
-      </div>
-    </>
+              ))}
+        </div>
+      </td>
+    </tr>
   );
 }
 export default Dashboard;
